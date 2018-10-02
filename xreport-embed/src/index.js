94443c88732c4230f08a53fbdd4f669547018bd9
Ahmed Harmouche <wpmed92@admins-MacBook-Pro-2.local>
2018-10-02 13:25:13 +0200

    Promisify makeWidget

--- xreport-embed/src/index.js
+++ xreport-embed/src/index.js
@@ -5,11 +5,14 @@ import { XReportRenderer } from './xreport-render.js';
 const xreportDOM = new XReportDOM();
 const xreportRenderer = new XReportRenderer(xreportDOM);
 
-export function makeWidget(url, title, targetId) {
-  xreportDOM.load(url, function(dom, conditions) {
-    let form = xreportRenderer.render(dom, title, targetId);
-    let xreportEval = new XReportEvaluator(xreportDOM, conditions);
-    xreportEval.attachToForm(form);
+export function makeWidget(url, title, targetId, success) {
+  return new Promise((resolve, reject) => {
+    xreportDOM.load(url, function(dom, conditions) {
+      let form = xreportRenderer.render(dom, title, targetId);
+      let xreportEval = new XReportEvaluator(xreportDOM, conditions);
+      xreportEval.attachToForm(form);
+      resolve();
+    });
   });
 }
 
