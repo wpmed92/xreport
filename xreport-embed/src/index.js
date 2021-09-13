340fd415ea35516775a2c87b27b389f1d321de11
Ahmed Harmouche <ahmedharmouche92@gmail.com>
2021-09-13 21:09:52 +0200

    Docs fixes

--- xreport-embed/src/index.js
+++ xreport-embed/src/index.js
@@ -3,9 +3,21 @@ import { XReportRenderer } from './xreport-render.js';
 import { Evaluator } from './form-script/evaluator';
 import 'bootstrap';
 
+/**
+ * Main entry point of xreport-dom library.
+ * @module xreport-dom
+ */
 const xreportDOM = new XReportDOM();
 const xreportRenderer = new XReportRenderer(xreportDOM);
 
+/**
+ * Make an embedded template builder or viewer widget.
+ * @param {string} url - The URL of the template JSON.
+ * @param {string} title - The title of the template.
+ * @param {string} targetId - The id of the element that will host the widget.
+ * @param {boolean} editorMode - 'true' for builder, 'false' for viewer.
+ * @returns {Promise}
+ */
 export function makeWidget(url, title, targetId, editorMode) {
   return new Promise((resolve, reject) => {
     let xreportEval = new Evaluator(xreportDOM);
@@ -30,14 +42,25 @@ export function makeWidget(url, title, targetId, editorMode) {
   });
 }
 
+/**
+ * Toggles between showing the form and the text output.
+ */
 export function togglePreviewMode() {
   xreportRenderer.togglePreviewMode();
 }
 
+/**
+ * Get the generated report as text.
+ * @returns {string}
+ */
 export function getReportAsText() {
   return xreportRenderer.getReportAsText();
 }
 
+/**
+ * Get the template in JSON format.
+ * @returns {object}
+ */
 export function getTemplateForUpload() {
   return xreportRenderer.getTemplateAsPayload();
 }
